32cdcbcd0fa82ae1efd536a11cb96afde5beae3e
Alex Kowalenko <alex.kowalenko@gmail.com>
2024-01-17 19:39:36 +1100

    Reformat indent to 4 spaces.

--- subroutines.f90
+++ subroutines.f90
@@ -2,79 +2,79 @@
 
 
 SUBROUTINE SPEAK(index)
-   implicit none
-   integer, intent(in) :: index
+    implicit none
+    integer, intent(in) :: index
 
-   ! Globals
-   include 'common.f90'
+    ! Globals
+    include 'common.f90'
 
-   integer :: K
+    integer :: K
 
-   K = RTEXT(index)
-   IF (K == 0) RETURN
-   DO
-      PRINT '(A)', text_lines(K)
-      K = K+1
-      IF (text_lines_index(K-1) == 0) EXIT
-   END DO
-   PRINT '(/)'
+    K = RTEXT(index)
+    IF (K == 0) RETURN
+    DO
+        PRINT '(A)', text_lines(K)
+        K = K+1
+        IF (text_lines_index(K-1) == 0) EXIT
+    END DO
+    PRINT '(/)'
 END SUBROUTINE SPEAK
 
 
 SUBROUTINE GET_INPUT(two_words, word1, word2, D)
-   implicit none
-   logical, intent(out):: two_words
-   character(len=5), intent(out) :: word1, word2, D
+    implicit none
+    logical, intent(out):: two_words
+    character(len=5), intent(out) :: word1, word2, D
 
-   integer, parameter :: lower_a =  ICHAR('a')
-   integer, parameter :: upper_a =  ICHAR('A')
-   character :: c
+    integer, parameter :: lower_a =  ICHAR('a')
+    integer, parameter :: upper_a =  ICHAR('A')
+    character :: c
 
-   character(len=100) :: LINE
-   integer :: A(5)
-   integer :: j
+    character(len=100) :: LINE
+    integer :: A(5)
+    integer :: j
 
-   READ(*,1) LINE
-1  FORMAT(A)
+    READ(*,1) LINE
+1   FORMAT(A)
 
-   ! Convert to uppcase
-   DO  J=1,LEN(LINE)
-      c = LINE(J:J)
-      IF (c >= 'a' .AND. c <= 'z') LINE(J:J) = CHAR(ICHAR(c) - lower_a + upper_a)
-   END DO
+    ! Convert to uppcase
+    DO  J=1,LEN(LINE)
+        c = LINE(J:J)
+        IF (c >= 'a' .AND. c <= 'z') LINE(J:J) = CHAR(ICHAR(c) - lower_a + upper_a)
+    END DO
 
-   ! Separate out two words if present
-   two_words = .false.
+    ! Separate out two words if present
+    two_words = .FALSE.
 
-   word1=LINE(1:5)
-   D=LINE(6:10)
-   J=INDEX(word1,' ')
-   IF (J /= 0) word1(J:) = ' '
-   J = INDEX(D,' ')
-   IF (J /= 0) D(J:) = ' '
-   J = INDEX(LINE,' ')
-   IF (J > 2) word2 = LINE(J+1:)
-   IF (J > 2 .AND. word2 /= ' ') two_words = .true.
+    word1=LINE(1:5)
+    D=LINE(6:10)
+    J=INDEX(word1,' ')
+    IF (J /= 0) word1(J:) = ' '
+    J = INDEX(D,' ')
+    IF (J /= 0) D(J:) = ' '
+    J = INDEX(LINE,' ')
+    IF (J > 2) word2 = LINE(J+1:)
+    IF (J > 2 .AND. word2 /= ' ') two_words = .TRUE.
 END SUBROUTINE GET_INPUT
 
 
 SUBROUTINE YES(msg, yes_msg, no_msg, yesno)
-   implicit none
-   integer, intent(in) :: msg, yes_msg, no_msg
-   logical, intent(out) :: yesno
+    implicit none
+    integer, intent(in) :: msg, yes_msg, no_msg
+    logical, intent(out) :: yesno
 
-   logical :: junk
-   character(len=5) :: word1, junc
+    logical :: junk
+    character(len=5) :: word1, junc
 
-   yesno = .false.
-   call SPEAK(msg)
-   call GET_INPUT(junk, word1, junc, junc)
-   if (word1 == 'NO' .OR. word1 == 'N') then
-      if (no_msg /= 0) CALL SPEAK(no_msg)
-      return
-   else
-      yesno = .true.
-      if (yes_msg /= 0) CALL SPEAK(yes_msg)
-      return
-   end if
+    yesno = .false.
+    call SPEAK(msg)
+    call GET_INPUT(junk, word1, junc, junc)
+    if (word1 == 'NO' .OR. word1 == 'N') then
+        if (no_msg /= 0) CALL SPEAK(no_msg)
+        return
+    else
+        yesno = .true.
+        if (yes_msg /= 0) CALL SPEAK(yes_msg)
+        return
+    end if
 END SUBROUTINE YES
